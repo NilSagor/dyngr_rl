7ee5e75c8c7dab45c1b0c4fa4dc2a39ea5b5bc03
NilSagor <masum1201@gmail.com>
2026-04-29 18:35:21 +0800

    freedyg components layer implemented

---
+++ experiment_framework/src/models/freedyg_module/components/FeedForwardNet.py
@@ -0,0 +1,34 @@
+
+import torch 
+import torch.nn as nn
+import numpy as np
+
+
+class FeedForwardNet(nn.Module):
+
+    def __init__(self, input_dim: int, dim_expansion_factor: float, dropout: float = 0.0):
+        """
+        two-layered MLP with GELU activation function.
+        :param input_dim: int, dimension of input
+        :param dim_expansion_factor: float, dimension expansion factor
+        :param dropout: float, dropout rate
+        """
+        super(FeedForwardNet, self).__init__()
+
+        self.input_dim = input_dim
+        self.dim_expansion_factor = dim_expansion_factor
+        self.dropout = dropout
+
+        self.ffn = nn.Sequential(nn.Linear(in_features=input_dim, out_features=int(dim_expansion_factor * input_dim)),
+                                 nn.GELU(),
+                                 nn.Dropout(dropout),
+                                 nn.Linear(in_features=int(dim_expansion_factor * input_dim), out_features=input_dim),
+                                 nn.Dropout(dropout))
+
+    def forward(self, x: torch.Tensor):
+        """
+        feed forward net forward process
+        :param x: Tensor, shape (*, input_dim)
+        :return:
+        """
+        return self.ffn(x)
